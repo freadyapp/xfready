5d22b8b2ed237d1421f09e0a98126b4b13e2d697
robo-monk <filtap007@gmail.com>
2020-11-24 14:45:03 +0100

    add domain cred

--- third_party/readable.js
+++ third_party/readable.js
@@ -1,1 +1,95 @@
-var REGEXPS = { unlikelyCandidates: /-ad-|ai2html|banner|breadcrumbs|combx|comment|community|cover-wrap|disqus|extra|footer|gdpr|header|legends|menu|related|remark|replies|rss|shoutbox|sidebar|skyscraper|social|sponsor|supplemental|ad-break|agegate|pagination|pager|popup|yom-remote/i, okMaybeItsACandidate: /and|article|body|column|content|main|shadow/i }; function isNodeVisible(e) { return (!e.style || "none" != e.style.display) && !e.hasAttribute("hidden") && (!e.hasAttribute("aria-hidden") || "true" != e.getAttribute("aria-hidden") || e.className && e.className.indexOf && -1 !== e.className.indexOf("fallback-image")) } function isProbablyReaderable(e, a) { a || (a = isNodeVisible); var r = e.querySelectorAll("p, pre"), t = e.querySelectorAll("div > br"); if (t.length) { var i = new Set(r);[].forEach.call(t, function (e) { i.add(e.parentNode) }), r = Array.from(i) } var n = 0; return [].some.call(r, function (e) { if (!a(e)) return !1; var r = e.className + " " + e.id; if (REGEXPS.unlikelyCandidates.test(r) && !REGEXPS.okMaybeItsACandidate.test(r)) return !1; if (e.matches("li p")) return !1; var t = e.textContent.trim().length; return !(t < 140) && (n += Math.sqrt(t - 140)) > 20 }) } "object" == typeof module && (module.exports = isProbablyReaderable);
+var REGEXPS = {
+  // NOTE: These two regular expressions are duplicated in
+  // Readability.js. Please keep both copies in sync.
+  unlikelyCandidates: /-ad-|ai2html|banner|breadcrumbs|combx|comment|community|cover-wrap|disqus|extra|footer|gdpr|header|legends|menu|related|remark|replies|rss|shoutbox|sidebar|skyscraper|social|sponsor|supplemental|ad-break|agegate|pagination|pager|popup|yom-remote/i,
+  okMaybeItsACandidate: /and|article|body|column|content|main|shadow/i,
+  likelyDomains: /and|wikipedia|wiki|blog|medium|news/i,
+  likelyPathContents: /and|topic|articles|news|blog|read|doc|about|info|/i,
+};
+
+function isNodeVisible(node) {
+  // Have to null-check node.style and node.className.indexOf to deal with SVG and MathML nodes.
+  return (!node.style || node.style.display != "none")
+    && !node.hasAttribute("hidden")
+    //check for "fallback-image" so that wikimedia math images are displayed
+    && (!node.hasAttribute("aria-hidden") || node.getAttribute("aria-hidden") != "true" || (node.className && node.className.indexOf && node.className.indexOf("fallback-image") !== -1));
+}
+
+/**
+ * Decides whether or not the document is reader-able without parsing the whole thing.
+ * @param {Object} options Configuration object.
+ * @param {number} [options.minContentLength=140] The minimum node content length used to decide if the document is readerable.
+ * @param {number} [options.minScore=20] The minumum cumulated 'score' used to determine if the document is readerable.
+ * @param {Function} [options.visibilityChecker=isNodeVisible] The function used to determine if a node is visible.
+ * @return {boolean} Whether or not we suspect Readability.parse() will suceeed at returning an article object.
+ */
+function isProbablyReaderable(doc, options = {}) {
+  // For backward compatibility reasons 'options' can either be a configuration object or the function used
+  // to determine if a node is visible.
+  if (typeof options == "function") {
+    options = { visibilityChecker: options };
+  }
+
+  var defaultOptions = { minScore: 26, minContentLength: 200, visibilityChecker: isNodeVisible };
+  options = Object.assign(defaultOptions, options);
+
+  var nodes = doc.querySelectorAll("p, pre");
+
+  // Get <div> nodes which have <br> node(s) and append them into the `nodes` variable.
+  // Some articles' DOM structures might look like
+  // <div>
+  //   Sentences<br>
+  //   <br>
+  //   Sentences<br>
+  // </div>
+  var brNodes = doc.querySelectorAll("div > br");
+  if (brNodes.length) {
+    var set = new Set(nodes);
+    [].forEach.call(brNodes, function (node) {
+      set.add(node.parentNode);
+    });
+    nodes = Array.from(set);
+  }
+
+  var score = 0;
+  // This is a little cheeky, we use the accumulator 'score' to decide what to return from
+  // this callback:
+  return [].some.call(nodes, function (node) {
+    if (!options.visibilityChecker(node)) {
+      return false;
+    }
+
+    var matchString = node.className + " " + node.id;
+    if (REGEXPS.unlikelyCandidates.test(matchString) &&
+        !REGEXPS.okMaybeItsACandidate.test(matchString)) {
+      return false;
+    }
+
+    if (node.matches("li p")) {
+      return false;
+    }
+
+    var textContentLength = node.textContent.trim().length;
+    if (textContentLength < options.minContentLength) {
+      return false;
+    }
+
+    score += Math.sqrt(textContentLength - options.minContentLength) + addDomainCred(window.location)
+    console.log(`article's score: ${score}`)
+
+    if (score > options.minScore) {
+      return true;
+    }
+    return false;
+  });
+}
+
+function addDomainCred(location){
+  let cred = 4*REGEXPS.likelyDomains.test(location.host) + 2*REGEXPS.likelyPathContents.test(location.pathname)
+  console.log(`ADDING DOMAIN CRED FOR ${location}`, cred)
+  return cred
+}
+
+if (typeof module === "object") {
+  module.exports = isProbablyReaderable;
+}
